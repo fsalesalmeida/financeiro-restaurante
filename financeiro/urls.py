737696f78c34c0af3e59e7a292b334149df0af51
Matheus Mazoni <mmazoni.andrade@gmail.com>
2020-09-24 23:40:27 -0300

    limpando as configurações

--- financeiro/urls.py
+++ financeiro/urls.py
@@ -13,12 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import url
 from caixa.viewsets import DespesaByCaixaList, EntradaByCaixaList, SangriaByCaixaList
 from .routers import router
-from django.views.generic import TemplateView
 
 
 urlpatterns = [
@@ -26,6 +24,4 @@ urlpatterns = [
     url('^api/despesa/caixa/(?P<caixa_id>\w+)/$', DespesaByCaixaList.as_view()),
     url('^api/entrada/caixa/(?P<caixa_id>\w+)/$', EntradaByCaixaList.as_view()),
     url('^api/sangria/caixa/(?P<caixa_id>\w+)/$', SangriaByCaixaList.as_view()),
-    path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='index.html'), name='app'),
 ]
